238b8c33debfc1dc847f3f49d65bb1886f0ef844
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-29 12:11:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,12 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/^[aeiouAEIOU]/)
+  return true 
+end 
+false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.grep(/^un.*ing$/)
 end
 
 def words_five_letters_long(text)
