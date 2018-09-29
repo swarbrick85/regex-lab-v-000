723cbee3202af0b621a5a0edb16e8d5d5859e4f2
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-29 12:34:00 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/^[aeiouAEIOU]/)
+  return true 
+end 
+false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b\w\w\w\w\w\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/\A[A-Z].*[.?!]\z/)
+  return true
+end
+false
 end
 
 def valid_phone_number?(phone)
-
+if phone.match(/\A.*[0-9]{3}.*[0-9]{3}.*[0-9]{4}.*\z/)
+  return true
+end
+false
 end
