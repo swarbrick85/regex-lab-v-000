eb331932e1106301f8cd8dcdbee365cc6ff9d896
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-09-29 12:26:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/\A/)
+if text.match(/\A[A-Z].*[.?!]\z/)
 end
 
 def valid_phone_number?(phone)
